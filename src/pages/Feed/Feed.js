ad5d34c3a36c8de304441d55b3c1e192232ad50d
Sara Anwar <saraanwar97@gmail.com>
2021-06-28 20:32:12 +0200

    using & validating jwt

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -50,7 +50,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page) //fetch all posts from rest api
+    fetch('http://localhost:8080/feed/posts?page=' + page,{
+      headers:{
+        Authorization:'Bearer ' + this.props.token
+      }
+    }) //fetch all posts from rest api
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
